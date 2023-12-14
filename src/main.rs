1331eac1784d3d018fb013d62ae9c29ae2712469
Severiano Sisneros <seve.sisneros@protonmail.com>
2023-12-14 15:21:39 -0800

    updated cli
    Signed-off-by: Severiano Sisneros <seve.sisneros@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::io::BufReader;
-
+use std::io::{self, BufReader, BufWriter};
 use clap::Parser;
 use decoder::{decode_flat_files, stream_blocks};
 
@@ -11,19 +10,28 @@ struct Args {
     headers_dir: Option<String>,
     #[clap(short, long)]
     output: Option<String>,
+    #[clap(long)]
+    stream: bool, // Add a flag for streaming
 }
 
 fn main() {
     let args = Args::parse();
-    // let input = match args.input {
-    //     Some(input) => decoder::DecodeInput::Path(input),
-    //     None => decoder::DecodeInput::Reader(Box::new(std::io::stdin())),
-    // };
 
-    let reader = BufReader::with_capacity(64 * 2<<20, std::io::stdin().lock());
-    let result = stream_blocks(reader, std::io::stdout().lock());
-    // let blocks = decode_flat_files(input, args.output.as_deref(), args.headers_dir.as_deref())
-    //     .expect("Failed to decode files");
+    // Check if the stream flag is set
+    if args.stream {
+        let reader = BufReader::with_capacity(64 * 2 << 20, io::stdin().lock());
+        let writer = BufWriter::new(io::stdout().lock());
+        stream_blocks(reader, writer).expect("Failed to stream blocks");
+    } else {
+        // Use file paths or stdin/out as before
+        let input = match args.input {
+            Some(input) => decoder::DecodeInput::Path(input),
+            None => decoder::DecodeInput::Reader(Box::new(BufReader::new(io::stdin()))),
+        };
 
-    // println!("Total blocks: {}", blocks.len());
+        let blocks = decode_flat_files(input, args.output.as_deref(), args.headers_dir.as_deref())
+            .expect("Failed to decode files");
+
+        println!("Total blocks: {}", blocks.len());
+    }
 }
