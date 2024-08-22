a46d1482aa952b27cb6152c9c401b0058385db01
Joseph Livesey <joseph@semiotic.ai>
2024-08-22 14:31:11 -0400

    refactor: use sf-protos type conversions

--- src/transactions/tx_type.rs
+++ src/transactions/tx_type.rs
@@ -8,28 +8,7 @@ pub enum TransactionTypeError {
     Missing,
 }
 
-fn tx_to_reth_tx(tx_type: Type) -> TxType {
-    use TxType::*;
-    use Type::*;
-
-    match tx_type {
-        TrxTypeLegacy => Legacy,
-        TrxTypeAccessList => Eip2930,
-        TrxTypeDynamicFee => Eip1559,
-        TrxTypeBlob => todo!(),
-        TrxTypeArbitrumDeposit => unimplemented!(),
-        TrxTypeArbitrumUnsigned => unimplemented!(),
-        TrxTypeArbitrumContract => unimplemented!(),
-        TrxTypeArbitrumRetry => unimplemented!(),
-        TrxTypeArbitrumSubmitRetryable => unimplemented!(),
-        TrxTypeArbitrumInternal => unimplemented!(),
-        TrxTypeArbitrumLegacy => unimplemented!(),
-        TrxTypeOptimismDeposit => unimplemented!(),
-    }
-}
-
 pub fn map_tx_type(tx_type: &i32) -> Result<TxType, TransactionTypeError> {
     let tx_type = Type::try_from(*tx_type).map_err(|_| TransactionTypeError::Missing)?; // 1
-    let tx_type = tx_to_reth_tx(tx_type);
-    Ok(tx_type)
+    Ok(TxType::from(tx_type))
 }
